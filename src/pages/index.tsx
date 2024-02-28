23c1013a67c052ba0017de67330b440bb59683fe
eric valcik <eric.valcik@gmail.com>
2024-02-28 21:34:17 +0100

    scale down pdf

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,10 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <div className="w-full h-full flex flex-col items-center py-40">
-      <iframe src="/pdfresizer.com-pdf-resize-motr.pdf"
-              style={{ width: "1020px", height: "800px" }}></iframe>
+      <div className="overflow-clip rounded-3xl">
+        <iframe src="/vilgain-small.pdf"
+                style={{ width: "1020px", height: "800px" }}></iframe>
+      </div>
     </div>
   );
 }
