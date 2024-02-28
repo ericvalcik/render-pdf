fd8673923ac0e251135018f1c3719a6aaca51afa
eric valcik <eric.valcik@gmail.com>
2024-02-28 21:48:09 +0100

    mobile friendlier??

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,10 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <div className="w-full h-full flex flex-col items-center py-40">
-      <div className="overflow-clip rounded-3xl">
-        <iframe src="/vilgain-small.pdf"
-                style={{ width: "1020px", height: "800px" }}></iframe>
+      <div>
+        <div className="overflow-clip rounded-3xl">
+          <iframe src="/vilgain-small.pdf" className="w-[350px] md:w-[512px] xl:w-[1024px] relative h-[800px]"></iframe>
+        </div>
       </div>
     </div>
   );
