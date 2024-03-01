3486ea40e05ee15fdc8ca8a743603e6084699e42
eric valcik <eric.valcik@gmail.com>
2024-03-01 10:09:04 +0100

    use figma embed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,10 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return (
     <div className="w-screen overflow-clip h-screen flex flex-col items-center py-40 bg-stone-800">
       <div>
         <div className="overflow-clip rounded-3xl">
-          <iframe src="/vilgain-small.pdf" className="w-[350px] md:w-[512px] xl:w-[1024px] relative h-[400px] md:h-[800px] bg-white"></iframe>
+          <iframe style={{ border: "1px solid rgba(0, 0, 0, 0.1);" }} width="800" height="600" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FCAxWTh9f2r4BzvtVvhjZW9%2FVilgain---Symbol-Design%3Ftype%3Ddesign%26node-id%3D271%253A134%26mode%3Ddesign%26t%3DJrJbORhoARuOp2vP-1"
+                  allowFullScreen></iframe>
         </div>
       </div>
     </div>
