d942b93e97ef9a5c017f93c5780195c2d86fd6ba
eric valcik <eric.valcik@gmail.com>
2024-03-06 00:20:01 +0100

    hide link to figma file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,7 @@ export default function Home() {
     <div className="w-screen overflow-clip h-screen flex flex-col items-center py-40 bg-stone-800">
       <div>
         <div className="overflow-clip rounded-3xl">
-          <iframe style={{ border: "1px solid rgba(0, 0, 0, 0.1);" }} width="800" height="600" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FCAxWTh9f2r4BzvtVvhjZW9%2FVilgain---Symbol-Design%3Ftype%3Ddesign%26node-id%3D271%253A134%26mode%3Ddesign%26t%3DJrJbORhoARuOp2vP-1"
-                  allowFullScreen></iframe>
+          <iframe style={{ border: "1px solid rgba(0, 0, 0, 0.1)" }} width="800" height="450" src="https://www.figma.com/embed?embed_host=share&community_viewer=true&url=https%3A%2F%2Fwww.figma.com%2Ffile%2Fzo2TMQxuwG56PhyvgkOHHY%2FVilgain---Symbol-Design-(Copy)%3Ftype%3Ddesign%26node-id%3D5289%253A2%26mode%3Ddesign%26t%3DNJMNaB8jqjs7AFW2-1" allowFullScreen></iframe>
         </div>
       </div>
     </div>
